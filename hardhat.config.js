a72d62c6d8f8ec19e767269615342f5d481a3f24
Ali Mousavi <amousavig@icloud.com>
2024-10-21 03:18:14 +0330

    Added AIA network.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  defaultNetwork: "aiaTestnet",
+  networks: {
+    aiaTestnet: {
+      url: "https://aia-dataseed1-testnet.aiachain.org",
+      chainId: 0x528,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
